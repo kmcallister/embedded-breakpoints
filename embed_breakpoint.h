6a031fbb7c07a297967c4cd00c682800c10cbeab
Keegan McAllister <mcallister.keegan@gmail.com>
2012-01-19 07:56:30 -0500

    rename a #define
    This name is visible to user code, so it should be a bit more unique.

--- embed_breakpoint.h
+++ embed_breakpoint.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #if __SIZEOF_POINTER__ > 4
-#define ASM_PTR ".quad"
+#define EMBED_BREAKPOINT_PTR ".quad"
 #else
-#define ASM_PTR ".long"
+#define EMBED_BREAKPOINT_PTR ".long"
 #endif
 
 #define EMBED_BREAKPOINT \
     asm("0:"                              \
         ".pushsection embed-breakpoints;" \
-        ASM_PTR " 0b;"                    \
+        EMBED_BREAKPOINT_PTR " 0b;"       \
         ".popsection;")
